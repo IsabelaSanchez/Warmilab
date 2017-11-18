064cc34570a4afa78b01203bc05e3ace96439c12
Isabela Sanchez <natyst_26@hotmail.com>
2017-11-18 12:06:06 -0500

    agregue variables

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -6,7 +6,27 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'about.html'
 })
 export class AboutPage {
-  
+
+  nombre: string = "Isabela Sanchez";
+  ocupacion: string = "Student";
+  edad: number = "15";
+
+  datos: any = {
+    nombre: "Isabela Sanchez",
+    ocupacion: "Student",
+    edad: 15
+
+  };
+
+  grupo: any = {
+    origen: "Lima,Peru",
+    email: "natyst_26@hotmail.com",
+    telefono: "9876542"
+
+  }
+
+  };
+
   constructor(public navCtrl: NavController) {
 
   }
